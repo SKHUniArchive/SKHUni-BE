4bb8aebbad1e17edc291820b6869feb9ff429a74
giwoong <dev.choi0409@gmail.com>
2025-04-02 21:15:05 +0900

    first commit

---
+++ src/main/java/com/skhuni/skhunibackend/SkhuniBackendApplication.java
@@ -0,0 +1,13 @@
+package com.skhuni.skhunibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkhuniBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkhuniBackendApplication.class, args);
+    }
+
+}
