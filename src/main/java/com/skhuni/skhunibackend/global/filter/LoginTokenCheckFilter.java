7e7abe4422ef17e450acbdba2b76e78c0b0c81ff
giwoong <dev.choi0409@gmail.com>
2025-04-04 00:15:49 +0900

    feat(#6): 로그아웃기능 구현

--- src/main/java/com/skhuni/skhunibackend/global/filter/LoginTokenCheckFilter.java
+++ src/main/java/com/skhuni/skhunibackend/global/filter/LoginTokenCheckFilter.java
@@ -25,9 +25,15 @@ public class LoginTokenCheckFilter extends GenericFilterBean {
             "/api/token/access",
             "/swagger-ui/index.html",
             "/swagger-ui/swagger-initializer.js",
+            "/swagger-ui/swagger-ui.css",
+            "/swagger-ui/index.css",
+            "/swagger-ui/swagger-ui-standalone-preset.js",
+            "/swagger-ui/swagger-ui-bundle.js",
+            "/swagger-ui/favicon-32x32.png",
+            "/swagger-ui/favicon-16x16.png",
             "/swagger-ui/**",
-            "/v3/api-docs",
             "/v3/api-docs/swagger-config",
+            "/v3/api-docs",
             "/v3/api-docs/**"
     );
 
